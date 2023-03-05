35134c19a63fd8e1c7092891377ec91e94098389
Henry <henry-carlosz@hotmail.com>
2023-03-04 19:32:30 -0500

    modulo1 listo

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Producto
+# Clasico
+admin.site.register(Categoria)
+# admin.site.register(Producto)
 
-# Register your models here.
+# Usando decorador
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    # List_display=Campos que deseas que se muestre en el listado. Siempre se usa duplas (siempre con comas).
+    list_display = ('nombre', 'precio', 'categoria', 'fecha_registro')
+    list_editable = ('precio',)
